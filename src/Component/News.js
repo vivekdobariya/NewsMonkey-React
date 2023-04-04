5bb3b014aba5aaba2517e2f41ab02bd23bb51d14
vivekdobariya <vivekdobariya333@gmail.com>
2023-04-04 13:12:15 +0530

    v-34

--- src/Component/News.js
+++ src/Component/News.js
@@ -15,13 +15,19 @@ export class News extends Component {
         pageSize: PropTypes.number,
         category: PropTypes.string,
     }
-    constructor() {
-        super();
+
+    capitalizeFirstLetter = (string)=> {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
+
+    constructor(props) {
+        super(props);
         this.state = {
             articles: [],
             loading: false,
             page: 1
         }
+        document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsMonkey`;
     }
 
     async updateNews() {
@@ -53,7 +59,7 @@ export class News extends Component {
     render() {
         return (
             <div className="container my-3">
-                <h1 className="text-center" style={{ margin: "35px 0px" }}>NewsMonkey - Top Headlines</h1>
+                <h1 className="text-center" style={{ margin: "35px 0px" }}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
                 {this.state.loading && <Spinner />}
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
